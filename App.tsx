140dcfec179559d52fcf21712a4f540eb0bbef9b
ShehabMind <shehabbebo6060@gmail.com>
2021-12-10 23:16:55 +0200

    feat(axios): added Axios Api Fetching library

--- App.tsx
+++ App.tsx
@@ -9,36 +9,17 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StatusBar, Text, View} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
       <StatusBar />
       <View>
-        <Text>App</Text>
+        <Text>News musala</Text>
       </View>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
